3a73f9f7ca544677d7716faa1efc816f65d688d7
Marcelo Miney <32588950+marcelominey@users.noreply.github.com>
2017-11-21 19:46:36 -0200

    Adicionando o método Consultar() nas classes 'Animal' e 'ServicosProdutos'
    Adicionando a classe 'Validacao';
    Adicionando o método Consultar() nas classes 'Animal', 'ServicosProdutos'.

--- Dominio/Cliente.cs
+++ Dominio/Cliente.cs
@@ -2,15 +2,15 @@
 using System.IO;
 using System.Text;
 
-namespace ProgramaCAO.Sistema.Solucao.Dominio {
-    public class Cliente {
+namespace Dominio{
+    public class Cliente{
         public int Id { get; set; }
         public string Nome { get; set; }
         public string Cpf { get; set; }
         public string Telefone { get; set; }
         public Endereco End { get; set; }
-        public Cliente () { }
-        public Cliente (int id, string nome, string cpf, string telefone, Endereco endereco) {
+        public Cliente(){}
+        public Cliente(int id, string nome, string cpf, string telefone, Endereco endereco){
             this.Id = id;
             this.Nome = nome;
             this.Cpf = cpf;
